8c0a2ade067063340c668da570f9fd88e3be8c26
EthanWang <ethan980301>
2017-09-27 21:12:09 +0800

    修改登录界面

--- Yutai/Commands/Security/frmLogin.cs
+++ Yutai/Commands/Security/frmLogin.cs
@@ -4,44 +4,45 @@ using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
+using DevExpress.XtraEditors;
 
 namespace Yutai.Commands.Security
 {
-	public partial class frmLogin : System.Windows.Forms.Form
-	{
-        
+    public partial class frmLogin : XtraForm
+    {
+
         private IList ilist_0;
         private IList ilist_1;
         private IList ilist_2;
-        
-		public frmLogin()
-		{
-			this.InitializeComponent();
-		}
 
-	private void frmLogin_Load(object sender, EventArgs e)
-		{
-		}
+        public frmLogin()
+        {
+            this.InitializeComponent();
+        }
 
-		private void btnOK_Click(object sender, EventArgs e)
-		{
-			if (this.txtUser.Text.Trim().Length == 0)
-			{
-				System.Windows.Forms.MessageBox.Show("请输入登录用户名!");
-			}
-			else
-			{
-				//ORGStaffHelper oRGStaffHelper = new ORGStaffHelper("ORGSTAFF");
-				//if (oRGStaffHelper.ValidePassword(this.txtUser.Text.Trim(), this.txtPassword.Text))
-				//{
-				//	base.DialogResult = System.Windows.Forms.DialogResult.OK;
-				//	base.Close();
-				//}
-				//else
-				//{
-				//	System.Windows.Forms.MessageBox.Show("密码错误!");
-				//}
-			}
-		}
-	}
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (this.txtUser.Text.Trim().Length == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("请输入登录用户名!");
+            }
+            else
+            {
+                //ORGStaffHelper oRGStaffHelper = new ORGStaffHelper("ORGSTAFF");
+                //if (oRGStaffHelper.ValidePassword(this.txtUser.Text.Trim(), this.txtPassword.Text))
+                //{
+                //	base.DialogResult = System.Windows.Forms.DialogResult.OK;
+                //	base.Close();
+                //}
+                //else
+                //{
+                //	System.Windows.Forms.MessageBox.Show("密码错误!");
+                //}
+            }
+        }
+    }
 }
--- Yutai/Commands/Security/frmLogin.designer.cs
+++ Yutai/Commands/Security/frmLogin.designer.cs
@@ -6,90 +6,117 @@ using System.Windows.Forms;
 
 namespace Yutai.Commands.Security
 {
-	    partial class frmLogin
+    partial class frmLogin
     {
-		protected override void Dispose(bool bool_0)
-		{
-			if (bool_0 && this.container_0 != null)
-			{
-				this.container_0.Dispose();
-			}
-			base.Dispose(bool_0);
-		}
+        protected override void Dispose(bool bool_0)
+        {
+            if (bool_0 && this.container_0 != null)
+            {
+                this.container_0.Dispose();
+            }
+            base.Dispose(bool_0);
+        }
 
-	
-	private void InitializeComponent()
-		{
-			ComponentResourceManager componentResourceManager = new ComponentResourceManager(typeof(frmLogin));
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.txtPassword = new System.Windows.Forms.TextBox();
-			this.btnOK = new System.Windows.Forms.Button();
-			this.btnCancel = new System.Windows.Forms.Button();
-			this.txtUser = new System.Windows.Forms.TextBox();
-			base.SuspendLayout();
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(47, 12);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "用户名:";
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 39);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(47, 12);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "密  码:";
-			this.txtPassword.Location = new System.Drawing.Point(76, 37);
-			this.txtPassword.Name = "txtPassword";
-			this.txtPassword.PasswordChar = '*';
-			this.txtPassword.Size = new System.Drawing.Size(144, 21);
-			this.txtPassword.TabIndex = 3;
-			this.btnOK.Location = new System.Drawing.Point(88, 64);
-			this.btnOK.Name = "btnOK";
-			this.btnOK.Size = new System.Drawing.Size(56, 24);
-			this.btnOK.TabIndex = 4;
-			this.btnOK.Text = "确定";
-			this.btnOK.Click += new EventHandler(this.btnOK_Click);
-			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(160, 64);
-			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(56, 24);
-			this.btnCancel.TabIndex = 5;
-			this.btnCancel.Text = "取消";
-			this.txtUser.Location = new System.Drawing.Point(76, 9);
-			this.txtUser.Name = "txtUser";
-			this.txtUser.ReadOnly = true;
-			this.txtUser.Size = new System.Drawing.Size(144, 21);
-			this.txtUser.TabIndex = 11;
-			this.txtUser.Text = "admin";
-			this.AutoScaleBaseSize = new System.Drawing.Size(6, 14);
-			base.ClientSize = new System.Drawing.Size(236, 97);
-			base.Controls.Add(this.txtUser);
-			base.Controls.Add(this.btnCancel);
-			base.Controls.Add(this.btnOK);
-			base.Controls.Add(this.txtPassword);
-			base.Controls.Add(this.label2);
-			base.Controls.Add(this.label1);
-			base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			base.Icon = (System.Drawing.Icon)componentResourceManager.GetObject("$Icon");
-			base.MaximizeBox = false;
-			base.MinimizeBox = false;
-			base.Name = "frmLogin";
-			base.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "登录";
-			base.Load += new EventHandler(this.frmLogin_Load);
-			base.ResumeLayout(false);
-			base.PerformLayout();
-		}
 
-	
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button btnOK;
-		private System.Windows.Forms.Button btnCancel;
-		private System.Windows.Forms.TextBox txtPassword;
-		private System.Windows.Forms.TextBox txtUser;
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtUser = new DevExpress.XtraEditors.TextEdit();
+            this.txtPassword = new DevExpress.XtraEditors.TextEdit();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(709, 358);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(437, 149);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(239, 20);
+            this.txtUser.TabIndex = 13;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(437, 198);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(239, 20);
+            this.txtPassword.TabIndex = 14;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOK.BackgroundImage")));
+            this.btnOK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnOK.FlatAppearance.BorderSize = 0;
+            this.btnOK.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnOK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(437, 249);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(111, 34);
+            this.btnOK.TabIndex = 15;
+            this.btnOK.Text = "登 录";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCancel.BackgroundImage")));
+            this.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(574, 249);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 34);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "取 消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(6, 15);
+            this.ClientSize = new System.Drawing.Size(709, 358);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
         private Container container_0 = null;
+        private PictureBox pictureBox1;
+        private DevExpress.XtraEditors.TextEdit txtUser;
+        private DevExpress.XtraEditors.TextEdit txtPassword;
+        private Button btnOK;
+        private Button btnCancel;
     }
 }
